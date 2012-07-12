61422f8f8c73be79a360ee3367c795d443738560
Shad <shad.downey@gmail.com>
2012-07-11 23:22:36 -0700

    bash scripts now can run form anywhere

--- addRecord.sh
+++ addRecord.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-php -f /vh/my.dns/httpdocs/app/webroot/addRecord.php $1 $2 $3 $4
-/vh/my.dns/httpdocs/myDns.sh
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+php -f $DIR/app/webroot/addRecord.php $1 $2 $3 $4
+$DIR/myDns.sh
--- myDns.sh
+++ myDns.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-sudo chmod -R 777 /vh/my.dns/httpdocs/app/named
-php -f /vh/my.dns/httpdocs/app/webroot/dns.php
-sudo mv /var/www/vhosts/my.dns/httpdocs/app/named/zones/* /var/named/
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+sudo chmod -R 777 $DIR/app/named
+php -f $DIR/app/webroot/dns.php
+sudo mv $DIR/app/named/zones/* /var/named/
 sudo chown -R root:wheel /var/named/
 sudo chmod 644 /var/named/*
-sudo mv /var/www/vhosts/my.dns/httpdocs/app/named/named.conf /etc/
+sudo mv $DIR/app/named/named.conf /etc/
 sudo chown root:wheel /etc/named.conf
 sudo chmod 644 /etc/named.conf
 sudo rndc reload
