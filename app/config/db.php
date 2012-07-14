0885901f01b99e13b17d822d3432093a6cd35f46
Shad <shad.downey@gmail.com>
2012-07-13 19:00:07 -0700

    switched to sqlite db

--- app/config/db.php
+++ app/config/db.php
@@ -1,8 +1,5 @@
 <?php
 DBCFactory::setWriteInfo(array(
-	'type' => 'mysql',
-	'host' => '127.0.0.1',
-	'db' => 'dns_db',
-	'usr' => 'dns_user',
-	'pwd' => 'dns_password',
+	'type' => 'sqlite',
+	'file' => CACHE_DIR . 'dns.db',
 ));
